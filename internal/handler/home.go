1020218d23f97e427a911e541e26c3fd9e9ecf9b
Shreyas sk <shreyassk.dev@gmail.com>
2024-08-23 10:04:50 +0530

    refactor: HTML Layout Project strucutre update

--- internal/handler/home.go
+++ internal/handler/home.go
@@ -2,13 +2,20 @@ package handler
 
 import (
 	"github.com/VAISHAKH-GK/cloth-shop-website/utils"
-	"github.com/VAISHAKH-GK/cloth-shop-website/view"
+	"github.com/VAISHAKH-GK/cloth-shop-website/view/admin"
+	"github.com/VAISHAKH-GK/cloth-shop-website/view/user"
 	"github.com/labstack/echo/v4"
 )
 
 // Home handler
 func Home(c echo.Context) error {
-	var component = view.Index()
+	var component = user.Index()
+
+	return utils.Render(c, component)
+}
+
+func Admin(c echo.Context) error {
+	var component = admin.Index()
 
 	return utils.Render(c, component)
 }
